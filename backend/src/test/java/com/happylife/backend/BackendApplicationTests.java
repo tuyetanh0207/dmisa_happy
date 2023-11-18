84463b60f9291645ed6d4b6cad9886c1fed04d47
tuyetanh0207 <nttanh02@gmail.com>
2023-11-18 14:44:49 +0700

    create react-app & spring boot app

---
+++ backend/src/test/java/com/happylife/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happylife.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
