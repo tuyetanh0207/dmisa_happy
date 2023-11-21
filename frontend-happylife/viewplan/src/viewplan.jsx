e51305806e0786a0707369070f602fc825da2098
HoaiMinh <phamlehoaiminh2014@gmail.com>
2023-11-21 10:34:05 +0700

    install vitejs( react ) and tailwind with create foler viewplan

---
+++ frontend-happylife/viewplan/src/viewplan.jsx
@@ -0,0 +1,19 @@
+const viewplan = () => {
+  return (
+    <main className="relative">
+      Nav
+      <h1>Plan</h1>
+      <section>
+        Insurance PLan ( Product )
+      </section>
+      <section>
+        Contact service
+      </section>
+      <section>
+        Footer
+      </section>
+    </main>
+  )
+}
+
+export default viewplan
