e81184b29b47ef1b765ae133d5c8c1c1cca5569e
tuyetanh0207 <nttanh02@gmail.com>
2023-11-28 12:15:18 +0700

    merge footer and nav

--- frontend-happylife/src/components/nav.jsx
+++ frontend-happylife/src/components/nav.jsx
@@ -1,52 +1,36 @@
-//  import {Routes, Route, Link} from 'react-router-dom';
- import logoTitle from '../assets/logoTitle.png';
-
-const navigationLeft = [
-  { name: 'Home', href: '/home', current: true },
-  { name: 'About', href: '/aboutus', current: true },
-  { name: 'Plans', href: '/plan', current: true },
-  { name: 'Contact', href: '/contact', current: true }, 
-]
-
-const navigationRight =[
-  { name: 'Login', href: '/login', current: false },
-  { name: 'Signup', href: '/signup', current: false },
-]
-
-// const handleLogin =()=>{
-//   setLogin(true);
-//}
 const Nav = () => {
-    return (
-    <nav className="h-20 bg-custom-blue flex justify-between items-center px-8  border-[0.25px] border-blue-500">
-     <img src={logoTitle} alt="LOGO" className='ml-[226px]'></img>  
-     <div className='flex space-x-76px place-content-center text-white font-sans font-medium font text-xl'>
-      {navigationLeft.map((item)=>(
-        <a
-        key={item.name}
-        href={item.href}  
-        >
-        <button>{item.name}</button>
-        </a>
-      ))}
-     </div>
-     <div className="flex space-x-6 text-white font-sans font-medium text-xl items-center">
-        {navigationRight.map((item) => (
-          <a key={item.name} href={item.href} className='btnSignup'>
-            {item.name === 'Signup' ? (
-              <button className="bg-button-blue text-white px-4 py-2 rounded items-center mr-[268px]"
-              >
-              
-              {item.name}
-            </button>
-            ) : (
-              item.name
-            )}
-          </a>
-        ))}
-      </div>
-    </nav>
-  )
+  return (
+    <header>
+      <nav>
+        <img src={logo}></img>
+        Home
+        About
+        Plan 
+        Contact
+      </nav>
+    </header>
+  
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+    )
+  
 }
-
-export default Nav
