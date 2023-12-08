4c86c9e6ca9097f225cfeaa6d90bc512a4a7ce28
tuyetanh0207 <nttanh02@gmail.com>
2023-12-08 16:40:15 +0700

    demo api

--- frontend-happylife/api/generic.js
+++ frontend-happylife/api/generic.js
@@ -5,28 +5,29 @@ export const apiV1 = `${baseUrl}/api/v1`;
 
 export const config = function (token) {
     return {
-        headers: {
-            "content-type": "application/json",
-            "Tokens": `Bearer ${token}`,
-        },
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
     };
-}
+  };
 
-export const get = function (url, token) {
-    return new Promise((resolve, reject) => {
-        axios
+  export const get = function (url, token) {
+    return new Promise((resolve, reject) =>
+      axios
         .get(url, config(token))
-        .then((response) => {
-            // return data
-            return resolve({data: response.data});
+        .then((res) => {
+          // return data
+          return resolve({ data: res.data });
         })
-        .catch((error)=>{
-            //return error message
-            if(!error.response) return reject(error.message);
-            return reject(error.response.data.message);
+        .catch((err) => {
+          // return err message
+          if (!err.response) return reject(err.message);
+          return reject(err.response.data.message);
         })
-    });
-}
+    );
+  };
+  
 
 export const post = function (url,data,token) {
     return new Promise((resolve, reject) => {
