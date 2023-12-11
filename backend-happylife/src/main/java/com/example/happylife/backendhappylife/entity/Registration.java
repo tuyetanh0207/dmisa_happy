87bbedd5e4d19015981d28e6f15ceb8349a00db6
tuyetanh0207 <nttanh02@gmail.com>
2023-12-12 00:27:20 +0700

    add registration entity

--- backend-happylife/src/main/java/com/example/happylife/backendhappylife/entity/Registration.java
+++ backend-happylife/src/main/java/com/example/happylife/backendhappylife/entity/Registration.java
@@ -1,2 +1,19 @@
-package com.example.happylife.backendhappylife.entity;public class Registration {
+package com.example.happylife.backendhappylife.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Document(collection="Registrations")
+public class Registration {
+    @Id
+    private ObjectId registerId;
 }
+
