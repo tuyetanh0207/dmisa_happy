6219963b6ae758e4197bdf06b7d7dcc3c13fa296
PHAN THIỆN NHÂN <phanthiennhan248@gmail.com>
2023-12-15 04:28:33 +0700

    update structure of element signup to fit with api, but its still response bad request

--- frontend-happylife/src/pages/signup/signup.jsx
+++ frontend-happylife/src/pages/signup/signup.jsx
@@ -4,14 +4,17 @@ import {registerUser} from '../../../redux/authApi';
 import { useNavigate } from 'react-router-dom';
 
 const signup = () => {
-  const [username, setUsername] = useState('');
+    
+  const [phoneNumber, setPhoneNumber] = useState('');
+  //const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [fullName, setfullName] = useState('');
+  const [fullName, setFullName] = useState('');
   const [gender, setGender] = useState('');
   const [email, setEmail] = useState('');
   const [dob, setDob] = useState('');
   const [address, setAddress] = useState('');
   const [citizenID, setCitizenID] = useState('');
+
   const dispatch = useDispatch();
   const router = useNavigate();
   const [noti, setNoti] = useState('');
@@ -19,14 +22,14 @@ const signup = () => {
     e.preventDefault();
     console.log('signup')
     const newUser = {
-        phoneNumber: username,
+        phoneNumber: phoneNumber,
         password: password,
         fullName: fullName,
         gender: gender,
         email: email,
         dob: dob,
         address: address,
-        citizenID: citizenID
+        citizenId: citizenID
     }
     console.log('new User', newUser)
     try{
@@ -41,7 +44,7 @@ const signup = () => {
     
   return (
     <div className="h-screen flex items-center justify-center h-[631px] bg-bgr-white my-auto">
-        <div className="w-[936px] h-[780px] bg-white rounded-lg">
+        <div className="w-[936px] h-[980px] bg-white rounded-lg">
         <h2 className="text-center text-header-blue text-[40px] font-serif font-semibold mt-[51px] mb-[53px]">Registration Form</h2>
         <form className="font-sans  font-medium text-base"
             onSubmit={handleSubmit}
@@ -53,20 +56,20 @@ const signup = () => {
             </div>
             <div className="flex items-center justify-center">
                 <input className="text-black w-[519px] h-[48px] mb-[15px] border border-input-border-grey border-solid rounded"
-                id='username'
-                onChange={(e)=>setUsername(e.target.value)}
+                id='phonenumber'
+                onChange={(e)=>setPhoneNumber(e.target.value)}
                 >
                 </input>
             </div>
             <div>
                 <label className="ml-[208px] ">
-                    Email
+                    Full Name
                 </label>
             </div>
             <div  className="flex items-center justify-center">
                 <input className="text-black w-[519px] h-[48px] mb-[12px] border border-input-border-grey rounded"
-                 id='email'
-                 onChange={(e)=>setEmail(e.target.value)}
+                 id='fullname'
+                 onChange={(e)=>setFullName(e.target.value)}
                 >
                 </input>
             </div>
@@ -83,25 +86,19 @@ const signup = () => {
                 </input>
             </div>
             <div>
-                <label className="ml-[208px] ">
-                    Confirm Password
-                </label>
-            </div>
-            <div  className="flex items-center justify-center">
-                <input className="text-black w-[519px] h-[48px] mb-[12px] border border-input-border-grey rounded">
-                </input>
-            </div>
-            <div>
                 <label className="ml-[208px]">
-                    Phone Number
+                    Gender 
                 </label>
-                <label className="ml-[155px]">
+                <label className="ml-[215px]">
                     Birthday
                 </label>
                 
             </div>
             <div>
-                <input className="text-black w-[253px] h-[48px] mb-[12px] ml-[208px] border border-input-border-grey rounded">
+                <input className="text-black w-[253px] h-[48px] mb-[12px] ml-[208px] border border-input-border-grey rounded"
+                id='gender'
+                onChange={(e)=>setGender(e.target.value)}
+                >
                 </input>
                 <input className="text-black w-[253px] h-[48px] mb-[12px] ml-[13px] border border-input-border-grey rounded"
                 id='dob'
@@ -111,7 +108,7 @@ const signup = () => {
             </div>
             <div>
                 <label className="ml-[208px] ">
-                    Identity Number
+                    Citizen ID
                 </label>
             </div>
             <div  className="flex items-center justify-center">
@@ -121,11 +118,35 @@ const signup = () => {
                 >
                 </input>
             </div>
+            <div>
+                <label className="ml-[208px] ">
+                    Email
+                </label>
+            </div>
+            <div  className="flex items-center justify-center">
+                <input className="text-black w-[519px] h-[48px] mb-[38px] border border-input-border-grey rounded"
+                 id='email'
+                 onChange={(e)=>setEmail(e.target.value)}
+                >
+                </input>
+            </div>
             
+            <div>
+                <label className="ml-[208px] ">
+                    Address
+                </label>
+            </div>
+            <div  className="flex items-center justify-center">
+                <input className="text-black w-[519px] h-[48px] mb-[38px] border border-input-border-grey rounded"
+                 id='address'
+                 onChange={(e)=>setAddress(e.target.value)}
+                >
+                </input>
+            </div>
 
             <div className="flex items-center justify-center font-bold text-xl">
             <button className="bg-button-blue w-[520px] h-[56px] text-white rounded-lg"
-            onClick={()=>handleSubmit}
+            //onClick={()=>handleSubmit}
             >
                 Create
             </button>
