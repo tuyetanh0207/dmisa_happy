3c31e8fc398e7888af300d940522546bf7c0ca24
tuyetanh0207 <nttanh02@gmail.com>
2024-01-18 22:43:39 +0700

    ..

---
+++ backend-happylife/src/main/java/com/example/happylife/backendhappylife/DTO/NotificationDTO/NotificationCreateDTO.java
@@ -0,0 +1,59 @@
+package com.example.happylife.backendhappylife.DTO.NotificationDTO;
+
+import com.example.happylife.backendhappylife.DTO.UserDTO.UserResDTO;
+import jakarta.persistence.Column;
+import lombok.Getter;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+
+import java.time.Instant;
+
+@Getter
+public class NotificationResDTO {
+    private String notificationId;
+    private ObjectId userInfo;
+    private String notiTitle;
+    private String notiContent;
+    private Boolean notiStatus;
+    private String notiType;
+    private String notiPrio;
+    private Instant CreatedAt;
+    private Instant UpdatedAt;
+
+    public void setNotificationId(String notificationId) {
+        this.notificationId = notificationId;
+    }
+
+    public void setUserInfo(ObjectId userInfo) {
+        this.userInfo = userInfo;
+    }
+
+    public void setNotiTitle(String notiTitle) {
+        this.notiTitle = notiTitle;
+    }
+
+    public void setNotiContent(String notiContent) {
+        this.notiContent = notiContent;
+    }
+
+    public void setNotiStatus(Boolean notiStatus) {
+        this.notiStatus = notiStatus;
+    }
+
+    public void setNotiType(String notiType) {
+        this.notiType = notiType;
+    }
+
+    public void setNotiPrio(String notiPrio) {
+        this.notiPrio = notiPrio;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        CreatedAt = createdAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        UpdatedAt = updatedAt;
+    }
+}
