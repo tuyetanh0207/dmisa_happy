c08d2e7cdd3f182f5e64cde3ada29f742a0d3e27
MSPhucNgo <huuphuc1256@gmail.com>
2024-01-19 16:27:06 +0700

    commit for totalfee

--- backend-happylife/src/main/java/com/example/happylife/backendhappylife/DTO/RegistrationDTO/RegisCreateDTO.java
+++ backend-happylife/src/main/java/com/example/happylife/backendhappylife/DTO/RegistrationDTO/RegisCreateDTO.java
@@ -3,10 +3,7 @@ package com.example.happylife.backendhappylife.DTO.RegistrationDTO;
 import com.example.happylife.backendhappylife.DTO.PlanDTO.PlanResDTO;
 import com.example.happylife.backendhappylife.DTO.UserDTO.UserResDTO;
 import com.example.happylife.backendhappylife.entity.Registration;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.Instant;
 import java.util.List;
@@ -16,75 +13,57 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisCreateDTO {
+    @Getter
     private String regisId;
+    @Getter
     private UserResDTO customerInfo;
+    @Getter
     private PlanResDTO productInfo;
+    @Getter
     private UserResDTO managerInfo;
 
+    private Integer totalFee;
+    @Getter
     private String approvalStatus;
+    @Getter
     private Instant startDate;
+    @Getter
     private Instant endDate;
+    @Getter
     private List<Registration.documentRegiss> documentUrls;
 
-    public String getRegisId() {
-        return regisId;
+    public void setTotalFee(Integer totalFee) {
+        this.totalFee = totalFee;
     }
 
     public void setRegisId(String regisId) {
         this.regisId = regisId;
     }
 
-    public UserResDTO getCustomerInfo() {
-        return customerInfo;
-    }
-
     public void setCustomerInfo(UserResDTO customerInfo) {
         this.customerInfo = customerInfo;
-    }
-
-    public PlanResDTO getProductInfo() {
-        return productInfo;
     }
 
     public void setProductInfo(PlanResDTO productInfo) {
         this.productInfo = productInfo;
     }
 
-    public UserResDTO getManagerInfo() {
-        return managerInfo;
-    }
-
     public void setManagerInfo(UserResDTO managerInfo) {
         this.managerInfo = managerInfo;
-    }
-
-    public String getApprovalStatus() {
-        return approvalStatus;
     }
 
     public void setApprovalStatus(String approvalStatus) {
         this.approvalStatus = approvalStatus;
     }
 
-    public Instant getStartDate() {
-        return startDate;
-    }
-
     public void setStartDate(Instant startDate) {
         this.startDate = startDate;
-    }
-
-    public Instant getEndDate() {
-        return endDate;
     }
 
     public void setEndDate(Instant endDate) {
         this.endDate = endDate;
     }
 
-    public List<Registration.documentRegiss>  getDocumentUrls() {
-        return documentUrls;
-    }
     public void setDocumentUrls(List<Registration.documentRegiss> documentUrls) {
         this.documentUrls = documentUrls;
     }
