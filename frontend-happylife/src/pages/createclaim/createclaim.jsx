6769db8c3c247549c90487a3a23b36ea5f9f2906
HoaiMinh <phamlehoaiminh2014@gmail.com>
2024-01-20 20:48:20 +0700

    add amount and upload file and status

--- frontend-happylife/src/pages/createclaim/createclaim.jsx
+++ frontend-happylife/src/pages/createclaim/createclaim.jsx
@@ -11,39 +11,25 @@ export default function CreateClaim() {
     const [registrations, setRegistrations] = useState(null);
     const [claims, setClaims] = useState(null);
     const [content,setContent] =useState('');
+    const [amount,setAmount] =useState(0); 
     const [hospitalName,setHospitalName] =useState('');
     const categoriesOption = [
-        {value:"Hỗ trợ y tế",label:"Hỗ trợ y tế"},
+        {value:"Bảo hiểm sức khỏe căn bản",label:"Bảo hiểm sức khỏe căn bản"},
+        {value:"Bảo hiểm sức khỏe thông dụng",label:"Bảo hiểm sức khỏe thông dụng"},
+        {value:"Bảo hiểm sức khỏe nâng cao",label:"Bảo hiểm sức khỏe nâng cao"},
+        {value:"Bảo hiểm sức khỏe đặc biệt",label:"Bảo hiểm sức khỏe đặc biệt"},
         {value:"Điều trị ngoại trú",label:"Điều trị ngoại trú"},
         {value:"Điều trị nha khoa",label:"Điều trị nha khoa"},
         {value:"Sinh mạng cá nhân",label:"Sinh mạng cá nhân"},
         {value:"Tai nạn cá nhân",label:"Tai nạn cá nhân"},
 
     ];
+    
     //claim
     const [claimCategories,setClaimCategories] =useState([]);
     const fetchRegistrations = async () => {
-        try{
-          //console.log('token', user.token)
-          const res = await RegistrationAPI.getAllRegistration(user.token);
-          let data= res.data
-          
-          const sortedArray = data.sort((a, b) => {
-            // Assuming createdAt is a string in ISO 8601 format, you can directly compare them
-            return new Date(b.createdAt) - new Date(a.createdAt);
-          });
-          setRegistrations(sortedArray)
-
-
-          
-          //console.log('res data', res.data);
-        } catch(err){
-          console.log('error in fetchRegistrations', err);
-        }
-        
-    }
-    const fetchClaims =async () => {
-        const url = 'http://localhost:8090/api/v1/claims';
+        console.log('userID:',user.userInfo.id)
+        const url =`http://localhost:8090/api/v1/registrations/${user.userInfo.id}`
         axios.get(url, {
             headers: {
                 'Content-Type': 'application/json',
@@ -51,7 +37,24 @@ export default function CreateClaim() {
             }
         })
             .then(response => {
-                console.log('Claim', response.data);
+                console.log('Regis:', response.data);
+                setRegistrations(response.data)
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+        
+    }
+    const fetchClaims =async () => {
+        const url = `http://localhost:8090/api/v1/claims/${user.userInfo.id}`;
+        axios.get(url, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${user.token}`
+            }
+        })
+            .then(response => {
+                console.log('Claim:', response.data);
                 setClaims(response.data)
             })
             .catch(error => {
@@ -59,7 +62,24 @@ export default function CreateClaim() {
             });
         
     }
+    const [files, setFiles] = useState([]);
+    const [isImageFile, setIsImageFile] = useState(false);
+    let url1;
+
+
+  const handleFileChange = (event) => {
+    const inputFile = event.target.files;
+    console.log('inputfile:',inputFile);
+    console.log('inputfile:',inputFile[0].name);
+    if (inputFile[0].name.includes('.pdf') == true){
+        console.log('inputfile:',inputFile[0].name.includes('.pdf'));
+        setIsImageFile(false);
+    } else {
+        setIsImageFile(true);
+    }
+    setFiles(inputFile);
     
+  };
     const handleClaimCategoriesChange = (selectCategories) => {
         // event.preventDefault()
         // try{
@@ -94,50 +114,95 @@ export default function CreateClaim() {
     const handleSubmit = async(e)=>{
         e.preventDefault();
         console.log("//////////////////////////////////////////////////////////")
-        try {
-          const regisID = "65a157175e0d7f1fdb6f512d"; 
+        
+          const regisID = "65abb268b85d687fe9d64067"; 
           const selectedRegistration = registrations.find(registration => registration.regisId === regisID);
-      
+            console.log('Select Regis:',selectedRegistration)
           if (!selectedRegistration) {
             console.error("Selected registration not found");
             return;
           }
       
-          const Regis = {
+          const claim = {
             regisInfo: selectedRegistration,
             claimCategories: claimCategories,
             content: content,
-            documentUrls: [
+            claimInvoices: [
                 {
-                    "docCategory": "Category1",
-                    "urls": [
-                        "http://url1.com",
-                        "http://url2.com"
-                    ]
+                    amount: amount,
+                    status: "Pending"
                 }
             ],
             hospitalName: hospitalName,
           };
-      
-          const url = 'http://localhost:8090/api/v1/claims/create';
-          const response = await axios.post(url, Regis, {
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${user.token}`
-            }
-          });
-      
-          console.log('Success:', response.data);
-        } catch (error) {
-          console.error('Error:', error);
-        }
-        };
 
+        const url = 'http://localhost:8090/api/v1/claims/create';
+        axios.post(url, claim, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${user.token}`
+        }
+        })
+        .then(response => {
+            console.log('Success:', response.data);
+            const newClaimId = response.data.claimId;
+
+            //console.log('Current: ',curentRegistrations)
+            console.log('123:', newClaimId);
+
+                    const fileCounts = [{section:"Insurant:",fileCount:files.length}];
+
+                    console.log('true/false: ',isImageFile)
+                    if(isImageFile==true){
+                        url1 =`http://localhost:8090/api/v1/claims/update/${newClaimId}/image-docUrl`;
+                    }
+                    else {
+                        url1 =`http://localhost:8090/api/v1/claims/update/${newClaimId}/file-docUrl`;
+                    }
+                    console.log('URL: ',url1)
+                    const formData = new FormData();
+
+                    formData.append('fileCounts', JSON.stringify(fileCounts))
+                    for (let i = 0; i < files.length; i++) {
+
+                      formData.append('files', files[i]);
+                    }
+
+                    //console.log('file url: ',fileURL)
+                    axios.put(url1, formData, 
+                    {
+                      headers: {
+                        'Content-Type': 'multipart/form-data',
+
+                      }
+                    })
+                      .then(response => {
+                        console.log('Success:', response.data);
+                        console.log('file: ',files);
+                        console.log('FormData: ',formData)
+                        // Handle the response as needed
+                      })
+                      .catch(error => {
+                        console.error('Error:', error);
+                        console.log('file: ',files);
+                        console.log('FormData: ',formData)
+                        // Handle errors
+                      });
+                   
+  
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+       
+        }
+    
         useEffect(() => {
             fetchRegistrations();
             fetchClaims();
            
         }, []); 
+    
     return(
         <div className=" bg-custom-blue-3 ">
         
@@ -160,14 +225,8 @@ export default function CreateClaim() {
                         </div>
                   </div>
                   <div className="pt-10 ">
-                            {/* {claims?.map((item, index) => (
-                                <div key={index} className=" text-center">
-                                    <button value={item.claimCategories}  onClick={handleClaimCategoriesChange} className= {`border-gray-600 border-2 rounded-lg w-full h-full `}  >
-                                        {item.claimCategories}
-                                    </button>
-                                </div>
-                            ))} */}
-                            <Select options={categoriesOption} onChange={handleClaimCategoriesChange} isMulti/>
+
+                        <Select options={categoriesOption} onChange={handleClaimCategoriesChange} isMulti/>
                     </div>
                     {/* <div className="flex flex-row items-center ">
                         {categoriesOption?.map((item,index) => (
@@ -197,6 +256,23 @@ export default function CreateClaim() {
                             onChange={(e)=>setHospitalName(e.target.value)}
                         />
                     </div>
+                    <div className="sm:col-span-full">   
+                        <label className="block text-xl font-medium leading-6 text-gray-900">
+                            Lượng tiền muốn bồi thường
+                        </label>
+                        <div className="mt-2">
+                        <input
+                            type="text"
+                            name="amount"
+                            id="amount"
+                            className="block w-full h-10 px-4 border-0 py-2 text-lg text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 placeholder:text-xl focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            placeholder='Your reason'
+                            onChange={(e)=>setAmount(e.target.value)}
+                        />
+                        </div>
+                    </div>
+
+                    <input type="file" onChange={handleFileChange} multiple />
                     <div className="flex items-center justify-between">
                             <div></div>
                             <button  className="px-32 py-6 text-2xl flex flex-row bg-indigo-50 rounded border font-bold font-['IBM Plex Sans'] text-custom-blue-3 border-indigo-500">
@@ -212,7 +288,7 @@ export default function CreateClaim() {
 
                                                 
           {/* test get registion */}
-          {registrations?.map((item, index) => (
+          {/* {registrations?.map((item, index) => (
             <div key={index}>
                 <div className="border-t border-gray-300 pl-8 pr-2 py-2">{index + 1}</div>
                 <div >{item.customerInfo.fullName}</div>
@@ -225,7 +301,7 @@ export default function CreateClaim() {
                 <div >{item.productInfo.planDuration + " " + item.productInfo.planDurationUnit + "s"}</div>
                 
             </div>
-            ))}
+            ))} */}
 
             
         </div>
