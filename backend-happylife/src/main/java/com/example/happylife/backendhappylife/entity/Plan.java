76d1fdfe29c5916a4e8fa207e420e60954d27115
MSPhucNgo <huuphuc1256@gmail.com>
2024-01-21 17:28:44 +0700

    commit for check email

--- backend-happylife/src/main/java/com/example/happylife/backendhappylife/entity/Plan.java
+++ backend-happylife/src/main/java/com/example/happylife/backendhappylife/entity/Plan.java
@@ -96,7 +96,7 @@ public class Plan {
         private String dependencies;
         private List<fees> feeType;
         private String unit;
-        private Integer insuranceAmount; //Là tiền quyền lợi người dùng nhận được khi mua gói này
+        private String insuranceAmount; //Là tiền quyền lợi người dùng nhận được khi mua gói này
 
         public void setBenefitName(String benefitName) {
             this.benefitName = benefitName;
@@ -114,7 +114,7 @@ public class Plan {
             this.unit = unit;
         }
 
-        public void setInsuranceAmount(Integer insuranceAmount) {
+        public void setInsuranceAmount(String insuranceAmount) {
             this.insuranceAmount = insuranceAmount;
         }
     }
