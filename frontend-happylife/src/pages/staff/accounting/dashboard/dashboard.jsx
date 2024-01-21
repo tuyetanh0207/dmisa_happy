375aa65b7fd74470ecab4f08c0ff5433a8072224
vuongtanphat139 <73147318+vuongtanphat139@users.noreply.github.com>
2024-01-21 11:16:25 +0700

    Get data Dashboard

--- frontend-happylife/src/pages/staff/accounting/dashboard/dashboard.jsx
+++ frontend-happylife/src/pages/staff/accounting/dashboard/dashboard.jsx
@@ -1,94 +1,58 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import ClaimAPI from "../../../../../api/claimApi";
-import AppButton from "../../../../components/staff/appButton/button";
-import StatusFilter from "../../../../components/staff/filter/status/statusFilter";
-import ClaimManagerPopup from "../../../../components/staff/popup/claimManagerPopup";
-import { statusArrayOfClaim } from "../../../../resource/status";
-import gStyles from "../../../../style";
-import { createMessageForClaim } from "../../../../supportFunctions";
+import StatistaAPI from "../../../../../api/statistaApi";
+
 const AccDashboard = () => {
   const user = useSelector((state) => state.auth.login.currentUser);
-  const [Claims, setClaims] = useState(null);
-  const [loadingBtns, setLoadingBtns] = useState([]);
-  const [filterStatus, setFilterStatus] = useState("All");
 
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
+  const [statista, setStatista] = useState({});
 
-  const fetchClaims = async () => {
+  // const [totalAvenueFromInsuranceFee, setTotalAvenueFromInsuranceFee] = useState({});
+  // const [totalProfit, setTotalProfit] = useState({});
+
+  // const [numOfClaim, setNumOfClaim] = useState({});
+  // const [numOfPendingClaim, setNumOfPendingClaim] = useState({});
+  // const [numOfResolvedClaim, setNumOfResolvedClaim] = useState({});
+  // const [totalClaimAmount, setTotalClaimAmount] = useState({});
+
+  // const [numOfActiveRegistration, setNumOfActiveRegistration] = useState({});
+  // const [numOfExpiredRegistration, setNumOfExpiredRegistration] = useState({});
+  // const [numOfInsuranceRegistration, setNumOfInsuranceRegistration] = useState({});
+
+  // const [compensationPayoutRatio, setCompensationPayoutRatio] = useState({});
+  // const [lossRatio, setLossRatio] = useState({});
+
+  const fetchStatista = async () => {
     try {
-      const res = await ClaimAPI.getAllClaim(user.token);
-      let data = res.data;
+      const res = await StatistaAPI.getStatista(user.token);
+      console.log(res);
 
-      if (!data || data[0].createdAt === null) {
-        setClaims(res.data);
-        return;
-      } else {
-        const sortedArray = data.sort((a, b) => {
-          // Assuming createdAt is a string in ISO 8601 format, you can directly compare them
+      setStatista(res?.data)
 
-          return new Date(b.createdAt) - new Date(a.createdAt);
-        });
-        setClaims(sortedArray);
-      }
+      // setLossRatio(res.data?.rateStatista.lossRatio);
+      // setCompensationPayoutRatio(res.data?.rateStatista.compensationPayoutRatio);
+
+      // setNumOfInsuranceRegistration(res.data?.insuranceStatista.numOfInsuranceRegistration);
+      // setNumOfExpiredRegistration(res.data?.insuranceStatista.numOfExpiredRegistration);
+      // setNumOfActiveRegistration(res.data?.insuranceStatista.numOfActiveRegistration);
+
+      // setTotalClaimAmount(res.data?.claimStatista.totalClaimAmount);
+      // setNumOfResolvedClaim(res.data?.numOfResolvedClaim);
+      // setNumOfPendingClaim(res.data?.claimStatista.numOfPendingClaim);
+      // setNumOfClaim(res.data?.claimStatista.numOfClaim);
+
+      // setTotalAvenueFromInsuranceFee(res.data?.avenueStatista.totalAvenueFromInsuranceFee);
+      // setTotalProfit(res.data?.avenueStatista.totalProfit);
+
     } catch (err) {
-      console.log("error in fetchClaims", err);
+      console.log("error in fetchStatista", err);
     }
   };
-  const [selectedRow, setSelectedRow] = useState(null);
-  const handleSelectingRow = (row) => {
-    setSelectedRow(row);
-  };
-  const handleClosePopup = () => {
-    setSelectedRow(null);
-  };
-  useEffect(() => {
-    fetchClaims();
-  }, []);
-  useEffect(() => {
-    setTimeout(() => {
-      fetchClaims();
-    }, 5000);
-  }, [Claims]);
 
-  const handleUpdateStatusOfClaim = async (claimId, status) => {
-    setLoadingBtns((t) => [...t, claimId]);
-    try {
-      await ClaimAPI.updateStatusOfClaim(
-        user.token,
-        claimId,
-        status,
-        { content: createMessageForClaim("", false, status) }
-      );
-      setLoadingBtns((t) => t.filter((id) => id !== claimId));
-    } catch (e) {
-      console.log("", e);
-    }
-  };
-  const colTitle = [
-    "No.",
-    "Cus. Name",
-    "Cus. Phone",
-    "Birthday",
-    "Address",
-    "Plan",
-    "Plan Type",
-    "Claim Amount",
-    "Total Request",
-    "Created At",
-    "Status",
-  ];
-  const handleChangeFilterStatus = (status) => {
-    setFilterStatus(status);
-  };
-  const handleChangeStartDateFilter = (date) => {
-    console.log("start date", date);
-    setStartDate(date);
-  };
-  const handleChangeEndDateFilter = (date) => {
-    setEndDate(date);
-  };
+  useEffect(() => {
+    fetchStatista();
+  },[]);
+
   return (
       <div className="bg-gray-100 font-sans">
         {/* Navigation */}
@@ -108,41 +72,45 @@ const AccDashboard = () => {
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
             {/* Card 1 */}
             <div className="bg-white p-4 rounded-lg shadow-md">
-              <h2 className="text-xl font-bold mb-4">Total Insurances</h2>
-              <p className="text-3xl font-bold text-green-500">$1,000,000</p>
+              <h2 className="text-xl font-bold mb-4">Total Claims</h2>
+              <p className="text-3xl font-bold text-green-500">{statista.claimStatista.numOfClaim}</p>
             </div>
 
             {/* Card 2 */}
             <div className="bg-white p-4 rounded-lg shadow-md">
-              <h2 className="text-xl font-bold mb-4">Total Canceled Insurances</h2>
-              <p className="text-3xl font-bold text-red-500">$500,000</p>
+              <h2 className="text-xl font-bold mb-4">Total Resolved Claims</h2>
+              <p className="text-3xl font-bold text-red-500">{statista.claimStatista.numOfResolvedClaim}</p>
             </div>
 
             {/* Card 3 */}
             <div className="bg-white p-4 rounded-lg shadow-md">
-              <h2 className="text-xl font-bold mb-4">Total Active Insurances</h2>
-              <p className="text-3xl font-bold text-blue-500">$500,000</p>
+              <h2 className="text-xl font-bold mb-4">Total Pending Claims</h2>
+              <p className="text-3xl font-bold text-blue-500">{statista.claimStatista.numOfPendingClaim}</p>
             </div>
 
             {/* Card 4 */}
             <div className="bg-white p-4 rounded-lg shadow-md">
-              <h2 className="text-xl font-bold mb-4">Compensation Statistics</h2>
-              <p className="text-3xl font-bold text-purple-500">$700,000</p>
+              <h2 className="text-xl font-bold mb-4">Total Claims Amount</h2>
+              <p className="text-3xl font-bold text-purple-500">{statista.claimStatista.totalClaimAmount}</p>
             </div>
           </div>
           </div>
   
-          {/* Section 2: Claims Statistics */}
+          {/* Section 2: Statistas Statistics */}
           <div className="mb-8">
-            <h2 className="text-2xl font-bold mb-4">Claims Statistics</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <h2 className="text-2xl font-bold mb-4">Registration Overview</h2>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
               <div className="bg-white p-4 rounded-lg shadow-md">
-                <h3 className="text-xl font-bold mb-2">Total Paid Claims</h3>
-                <p className="text-2xl text-green-500">$500,000</p>
+                <h3 className="text-xl font-bold mb-4">Total Insurance Registrations</h3>
+                <p className="text-3xl font-bold text-green-500">{statista.insuranceStatista.numOfInsuranceRegistration}</p>
               </div>
               <div className="bg-white p-4 rounded-lg shadow-md">
-                <h3 className="text-xl font-bold mb-2">Pending Claims</h3>
-                <p className="text-2xl text-red-500">30</p>
+                <h3 className="text-xl font-bold mb-4">Total Expired Registrations</h3>
+                <p className="text-3xl font-bold text-red-500">{statista.insuranceStatista.numOfExpiredRegistration}</p>
+              </div>
+              <div className="bg-white p-4 rounded-lg shadow-md">
+                <h3 className="text-xl font-bold mb-4">Total Active Registrations</h3>
+                <p className="text-3xl font-bold text-red-500">{statista.insuranceStatista.numOfActiveRegistration}</p>
               </div>
             </div>
           </div>
@@ -152,30 +120,30 @@ const AccDashboard = () => {
             <h2 className="text-2xl font-bold mb-4">Financial Information</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
               <div className="bg-white p-4 rounded-lg shadow-md">
-                <h3 className="text-xl font-bold mb-2">Insurance Revenue</h3>
-                <p className="text-2xl text-green-500">$1,200,000</p>
+                <h3 className="text-xl font-bold mb-4">Total Avenue From Insurance Fee</h3>
+                <p className="text-3xl font-bold text-green-500">{statista.avenueStatista.totalAvenueFromInsuranceFee}</p>
               </div>
               <div className="bg-white p-4 rounded-lg shadow-md">
-                <h3 className="text-xl font-bold mb-2">Profit/Loss</h3>
-                <p className="text-2xl text-blue-500">$300,000</p>
+                <h3 className="text-xl font-bold mb-4">Total Profit</h3>
+                <p className="text-3xl font-bold text-blue-500">{statista.avenueStatista.totalProfit}</p>
               </div>
             </div>
           </div>
   
-          {/* Section 4: Risk and Claims Ratio */}
-          <div>
-            <h2 className="text-2xl font-bold mb-4">Risk and Claims Ratio</h2>
+          {/* Section 4: Risk and Statistas Ratio */}
+          {/* <div>
+            <h2 className="text-2xl font-bold mb-4">Risk and Statistas Ratio</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
               <div className="bg-white p-4 rounded-lg shadow-md">
                 <h3 className="text-xl font-bold mb-2">Loss Ratio</h3>
-                <p className="text-2xl text-red-500">15%</p>
+                <p className="text-2xl text-red-500">{statista.rateStatista.lossRatio}</p>
               </div>
               <div className="bg-white p-4 rounded-lg shadow-md">
-                <h3 className="text-xl font-bold mb-2">Claims to Revenue Ratio</h3>
-                <p className="text-2xl text-purple-500">25%</p>
+                <h3 className="text-xl font-bold mb-2">Compensation Payout Ratio</h3>
+                <p className="text-2xl text-purple-500">{statista.rateStatista.compensationPayoutRatio}</p>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
   );
